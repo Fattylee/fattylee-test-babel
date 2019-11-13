70a436de22544b571ba0feca31eea09e7f801a97
Fatai Balogun <fattylee.remod@gmail.com>
2019-11-13 14:43:48 +0100

    update pk

--- dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-var _require = require('./util'),
-    sum = _require.sum;
-
-console.log('working!!', sum(23, 4));
--- uuid.js
+++
@@ -1,8 +0,0 @@
-const uuidv3 = require('uuid/v3');
-const uuid = require('uuid');
-
-
-console.log('DNS', uuidv3('dns',uuidv3.DNS));
-console.log('URL', uuidv3.URL);
-console.log('uuid', typeof uuid());
-console.log('=====')
