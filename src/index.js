8d63040a25fbe83f89846396cf627a0108141066
Fatai Balogun <fattylee.remod@gmail.com>
2019-11-13 23:28:05 +0100

    try with class and named export

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import sum from './sum';
+import sum, {age} from './sum';
 
 const name = 'fattylee';
 
@@ -6,5 +6,7 @@ console.log('people');
 
 const fn = (a, b) => a + b;
 
+
+
 console.log('sum of 2 + 3 is ', fn(2,3));
-console.log('sum of 12 + 3 is ', sum(12,3));
+console.log('sum of 12 + 3 is ', sum(12,3), age);
--- src/sum.js
+++ src/sum.js
@@ -1,3 +1,13 @@
 const sum = (a,b) => a + b;
 
+class lukman {
+  yourAge() {
+    return 'age here';
+  }
+}
+
+const luk = new lukman();
+
+export const age = luk.yourAge();
+
 export default sum;
