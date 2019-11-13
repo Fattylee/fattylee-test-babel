459d2e3e89ab49a78393a89a3c693a5f146cc4c0
Fatai Balogun <fattylee.remod@gmail.com>
2019-11-13 23:22:02 +0100

    add and import sum module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import sum from './sum';
+
 const name = 'fattylee';
 
 console.log('people');
@@ -5,3 +7,4 @@ console.log('people');
 const fn = (a, b) => a + b;
 
 console.log('sum of 2 + 3 is ', fn(2,3));
+console.log('sum of 12 + 3 is ', sum(12,3));
